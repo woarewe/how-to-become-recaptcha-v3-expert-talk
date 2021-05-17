7c5000436013e09fca6ad91b5638f799fc014225
woarewe <rostislav@hey.com>
2021-05-17 21:38:58 +0300

    Add validations

--- app/models/contact_inquiry.rb
+++ app/models/contact_inquiry.rb
@@ -1,2 +1,16 @@
 class ContactInquiry < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+
+  validate :email_format
+
+
+  private
+
+  def email_format
+    return if errors[:email].any?
+    return if URI::MailTo::EMAIL_REGEXP === email
+
+    errors.add(:email, "invalid format")
+  end
 end
